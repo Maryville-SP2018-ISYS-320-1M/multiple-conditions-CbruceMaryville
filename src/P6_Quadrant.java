d2a69b06593e307f54ac60518a2a374b07b2ee08
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 21:48:06 -0500

    Update P6_Quadrant.java

--- src/P6_Quadrant.java
+++ src/P6_Quadrant.java
@@ -1,13 +1,36 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/8/2018
 */
 
 public class P6_Quadrant {
 
 	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		
+		System.out.print("Give me your first number ");
+		int num1 = console.nextInt();
+		
+		System.out.print("Give me your second number ");
+		int num2 = console.nextInt();
+		
+		
+		if(num1 > 0 && num2 >0 ) {
+			System.out.println("This is in Q1");
+		}else if(num1 < 0 && num2 >0) {
+			System.out.println("This is in Q2");
 
+		}else if(num1 < 0 && num2 <0) {
+			System.out.println("This is in Q3");
+		}else if(num1 > 0 && num2 <0) {
+			System.out.println("This is in Q4");
+		}
+
+		
+		
 	}
 
 }
