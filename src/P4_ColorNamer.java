5d0bac0da64dbbdc324bbff39107b1622ec506b0
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 21:34:37 -0500

    Update P4_ColorNamer.java

--- src/P4_ColorNamer.java
+++ src/P4_ColorNamer.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/8/2018
 */
 
 public class P4_ColorNamer {
 
 	public static void main(String[] args) {
-
+		Scanner console = new Scanner(System.in);
+		
+		System.out.print("Give me a r,g,b for red, green or blue ");
+		String color = console.next();
+		String color2;
+		color2 = color.toUpperCase();
+		
+		if(color2.equals("R")) {
+			System.out.println("Red");
+		}else if (color2.equals("G")) {
+			System.out.println("Green");
+		}else if (color2.equals("B")) {
+			System.out.println("Blue");
+		}
+		
+		
 	}
 
 }
