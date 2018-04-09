8c9be3560daf30b90f6a6489535b016a990ca678
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 21:26:54 -0500

    Update P2_DonationMatcher.java

--- src/P2_DonationMatcher.java
+++ src/P2_DonationMatcher.java
@@ -10,22 +10,32 @@ public class P2_DonationMatcher {
 		System.out.print("Will you donation be multiplied 1 or 2? ");
 		int multiplier = console.nextInt();
 
+		
+		if(multiplier == 0 || multiplier <0 ) {
+			System.out.println("That is an invalid number: 0 or less" );
+		}
+		
+		
 		if (multiplier == 1) {
 			System.out.print("And how much are you donating? ");
 			int donation = console.nextInt();
 			donationsSum = donationsSum + donation;
 			donationsCount++;
-		}
-
-		if (multiplier == 2) {
+			System.out.println("Total donated sum: " + donationsSum);
+			System.out.println("Total number of donations: " + donationsCount);
+		}else if (multiplier == 2) {
 			System.out.print("And how much are you donating? ");
 			int donation = console.nextInt();
 			donationsSum = donationsSum + 2 * donation;
 			donationsCount++;
+			System.out.println("Total donated sum: " + donationsSum);
+			System.out.println("Total number of donations: " + donationsCount);
 		}
 
-		System.out.println("Total donated sum: " + donationsSum);
-		System.out.println("Total number of donations: " + donationsCount);
+	
+	
+	
+	
+	*/
 	}
-
 }
