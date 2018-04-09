a706c3d7db275b0b4ff2d757e01ec734d75fa453
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 21:42:23 -0500

    Update P5_ShapeChooser.java

--- src/P5_ShapeChooser.java
+++ src/P5_ShapeChooser.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/8/2018
 */
 
 public class P5_ShapeChooser {
 
 	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		
+		System.out.print("Give me tri for triangle or box for a box ");
+		String shape = console.next();
+		
+		if(shape.equals("tri")) {
+			drawTri();
+		}else if (shape.equals("box")) {
+			drawBox();
+		}else 
+		{
+			System.out.println("Error in input!");
+		}
 
+		
+		
+		
+		
 
 	}
 	
